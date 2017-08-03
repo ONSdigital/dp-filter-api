4942eb949b64a9d6207151e8bb0613a7dc72ff99
nshumoogum <nathan.shumoogum@methods.co.uk>
2017-08-03 19:17:10 +0100

    Update valid fields in request bodies
    Requirement change to store `dataset_filter_id` instead of `dataset`,
    `version` and `edition` as these are represented by the `instance_id`
    referred to here as `dataset_filter_id`.
    Also there is no need to store the json document sent in the initial
    POST request, hence being removed.

--- filterJobQueue/filterJobQueue_test.go
+++ filterJobQueue/filterJobQueue_test.go
@@ -12,7 +12,7 @@ func TestFilterJobQueue(t *testing.T) {
 	Convey("When a filter job is updated with a status `submitted`, a message is sent to kafka", t, func() {
 		filterJobQueue := make(chan []byte, 1)
 		jobQueue := CreateJobQueue(filterJobQueue)
-		filter := models.Filter{DataSet: "Census", Edition: "1997", Version: "1", State: "submitted", FilterID: "1234"}
+		filter := models.Filter{DataSetFilterID: "12345678", State: "submitted", FilterID: "1234"}
 		err := jobQueue.Queue(&filter)
 		So(err, ShouldBeNil)
 		bytes := <-filterJobQueue
