bb372216ef876bdca3add3bd06d530073bcab548
nshumoogum <nathan.shumoogum@methods.co.uk>
2017-08-11 16:16:01 +0100

    Fix broken tests due to variable name change in model
    `DataSetID` to `DatasetID`

--- mocks/datastore.go
+++ mocks/datastore.go
@@ -29,7 +29,7 @@ func (ds *DataStore) AddFilter(host string, filterJob *models.Filter) (models.Fi
 	if ds.InternalError {
 		return models.Filter{}, internalServerError
 	}
-	return models.Filter{DataSetFilterID: "12345678"}, nil
+	return models.Filter{DatasetFilterID: "12345678"}, nil
 }
 
 func (ds *DataStore) AddFilterDimension(dimension *models.AddDimension) error {
@@ -76,7 +76,7 @@ func (ds *DataStore) GetFilter(filterID string) (models.Filter, error) {
 	if ds.InternalError {
 		return models.Filter{}, internalServerError
 	}
-	return models.Filter{DataSetFilterID: "12345678"}, nil
+	return models.Filter{DatasetFilterID: "12345678"}, nil
 }
 
 func (ds *DataStore) GetFilterDimensions(filterID string) ([]models.Dimension, error) {
