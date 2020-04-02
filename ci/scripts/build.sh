8ceab5221f92988ab92d94b7be2d58dad35292a6
CarlHembrough <carlhuk@gmail.com>
2020-04-02 16:46:17 +0100

    Update CI scripts for Go module migration

--- ci/scripts/build.sh
+++ ci/scripts/build.sh
@@ -2,9 +2,7 @@
 
 cwd=$(pwd)
 
-export GOPATH=$cwd/go
-
-pushd $GOPATH/src/github.com/ONSdigital/dp-filter-api
+pushd $cwd/dp-filter-api
   make build && mv build/$(go env GOOS)-$(go env GOARCH)/* $cwd/build
   cp Dockerfile.concourse $cwd/build
 popd
--- ci/scripts/unit.sh
+++ ci/scripts/unit.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -eux
 
-export GOPATH=$(pwd)/go
+cwd=$(pwd)
 
-pushd $GOPATH/src/github.com/ONSdigital/dp-filter-api
+pushd $cwd/dp-filter-api
   make test
 popd
