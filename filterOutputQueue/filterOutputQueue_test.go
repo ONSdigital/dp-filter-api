09ded3d67c5f83acc27d21f8e3b8d10498de667d
nshumoogum <nathan.shumoogum@methods.co.uk>
2020-09-22 14:19:04 +0100

    Fix unit test - expecting dataset object

--- filterOutputQueue/filterOutputQueue_test.go
+++ filterOutputQueue/filterOutputQueue_test.go
@@ -12,7 +12,12 @@ func TestFilterOuputQueue(t *testing.T) {
 	Convey("When a filter output is created, a message is sent to kafka", t, func() {
 		filterOutputQueue := make(chan []byte, 1)
 		outputQueue := CreateOutputQueue(filterOutputQueue)
-		filter := models.Filter{InstanceID: "12345678", State: "submitted", FilterID: "1234"}
+		dataset := &models.Dataset{
+			ID:      "cpih01",
+			Edition: "2017",
+			Version: 1,
+		}
+		filter := models.Filter{InstanceID: "12345678", State: "submitted", FilterID: "1234", Dataset: dataset}
 		err := outputQueue.Queue(&filter)
 		So(err, ShouldBeNil)
 
