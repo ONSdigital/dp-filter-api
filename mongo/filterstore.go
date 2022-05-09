492eeb0150da952351db261e695122bee0ba4c61
Paulo Monteiro <paulo.from.portugal@gmail.com>
2022-05-09 11:40:48 +0100

    Search filterOutputs collection using 'filter_id'
    We are not able to search the `CMD` dataset at the moment, as we were
    searching for an `id` key instead of the `filter_id` key.
    
    Resolves: 5669

--- mongo/filterstore.go
+++ mongo/filterstore.go
@@ -379,7 +379,7 @@ func (s *FilterStore) CreateFilterOutput(ctx context.Context, filter *models.Fil
 func (s *FilterStore) GetFilterOutput(ctx context.Context, filterID string) (*models.Filter, error) {
 	// NOTE: previously was filter_id.
 	// This should be _id/id. Confirm during PR.
-	query := bson.M{"id": filterID}
+	query := bson.M{"filter_id": filterID}
 	var result *models.Filter
 
 	if err := s.Connection.Collection(s.ActualCollectionName(config.OutputsCollection)).FindOne(ctx, query, &result); err != nil {
